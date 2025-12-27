14f8413fe1c6b3c19e9d30b0a15d9f78b8faf231
Rattus <rattus128@gmail.com>
2025-12-27 11:20:58 +1000

    wip

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
-from setuptools import setup, Extension
 import os
+import sys
+from setuptools import setup, Extension
 
-cuda_include = "/usr/local/cuda-12.1/include"
-cuda_lib_stubs = "/usr/local/cuda-12.1/targets/x86_64-linux/lib/stubs"
-
-libvbar = Extension(
-    name="aimdo.libvbar",  # This will output libvbar.so inside the aimdo folder
-    sources=["src/aimdo.c"],
-    include_dirs=[cuda_include, "src"],
-    library_dirs=[cuda_lib_stubs],
-    libraries=["cuda"],
-    extra_compile_args=["-fPIC"],
-)
+if sys.platform == "win32":
+    # Windows paths (provided by the Jimver/cuda-toolkit action)
+    cuda_path = os.environ.get("CUDA_PATH", "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.1")
+    include_dirs = [os.path.join(cuda_path, "include")]
+    library_dirs = [os.path.join(cuda_path, "lib", "x64")]
+    libraries = ["cuda"]  # or "cudart" depending on what you need
+else:
+    # Your existing Linux paths
+    include_dirs = ["/usr/local/cuda-12.1/include"]
+    library_dirs = ["/usr/local/cuda-12.1/targets/x86_64-linux/lib/stubs"]
+    libraries = ["cuda"]
 
 setup(
-    ext_modules=[libvbar],
+    ext_modules=[
+        Extension(
+            name="aimdo.libvbar",
+            sources=["src/aimdo.c"],
+            include_dirs=include_dirs,
+            library_dirs=library_dirs,
+            libraries=libraries,
+        )
+    ],
 )
